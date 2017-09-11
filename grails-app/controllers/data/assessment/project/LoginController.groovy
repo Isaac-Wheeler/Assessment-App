8b3a59f4946dec0de5964dcc02c0fe502c64ee25
Isaac Wheeler <isaacwheeler0@gmail.com>
2017-09-11 12:37:19 -0400

    added ablity to log in and out

--- grails-app/controllers/data/assessment/project/LoginController.groovy
+++ grails-app/controllers/data/assessment/project/LoginController.groovy
@@ -2,5 +2,27 @@ package data.assessment.project
 
 class LoginController {
 
-    def index() { }
+    def index() { 
+     def username = prams.username
+     def password = prams.password
+     
+     if(username == "admin" && password == "pass"){
+     	flash.message = "login succeed"
+     	session.user = "admin"
+     }else{
+     	flash.message = "login failed"
+     }
+     
+     redirect(action: 'index')
+     
+    }
+    
+    def logout = {
+    	session.user = null
+    	redirect(action: 'index')
+    	
+    }
+    
 }
+
+	
