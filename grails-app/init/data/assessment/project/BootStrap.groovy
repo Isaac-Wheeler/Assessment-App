634273745ab0374d5734bef5be291a1fae9c93a6
Isaac Wheeler <isaacwheeler0@gmail.com>
2017-09-21 14:09:51 -0400

    added users

--- grails-app/init/data/assessment/project/BootStrap.groovy
+++ grails-app/init/data/assessment/project/BootStrap.groovy
@@ -3,6 +3,16 @@ package data.assessment.project
 class BootStrap {
 
     def init = { servletContext ->
+      if(User.count() == 0){
+       def u = new User(firstName:"admin", lastName:"DeleteMe", username:"admin", password:"password", admin:true)
+       u.passwordHashed = u.password.encodeAsPassword()
+       u.save()
+       System.out.println("added defualt admin")
+       u = new User(firstName:"teacher", lastName:"DeleteMe", username:"teacher", password:"password", admin:false)
+       u.passwordHashed = u.password.encodeAsPassword()
+       u.save()
+       System.out.println("added default teacher")
+      }
     }
     def destroy = {
     }
