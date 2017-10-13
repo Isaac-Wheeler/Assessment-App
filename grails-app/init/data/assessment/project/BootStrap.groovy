a9f5f55ea56502a70a2c14a8e21c1867ab81e90f
kbarley3 <kbarley3@ycp.edu>
2017-10-13 00:00:30 -0400

    Added environment fields to bootstrap and added default objects for test environment

--- grails-app/init/data/assessment/project/BootStrap.groovy
+++ grails-app/init/data/assessment/project/BootStrap.groovy
@@ -3,13 +3,41 @@ package data.assessment.project
 class BootStrap {
 
     def init = { servletContext ->
-      if(Teacher.count() == 0){
-       def u = new Teacher(firstName:"admin", lastName:"DeleteMe", username:"admin", password:"password", confirm:"password", admin:true)
-       u.passwordHashed = u.password.encodeAsPassword()
-       u.save()
-       System.out.println("added default admin")
-      }
+      environments {
+        production {
+
+
+        }
+        development {
+          if(Teacher.count() == 0){
+           def u = new Teacher(firstName:"admin", lastName:"DeleteMe", username:"admin", password:"password", confirm:"password", admin:true)
+           u.passwordHashed = u.password.encodeAsPassword()
+           u.save()
+           System.out.println("added default admin")
+          }
+
+        }
+        test {
+          if(Teacher.count() == 0){
+           def u = new Teacher(firstName:"admin", lastName:"DeleteMe", username:"admin", password:"password", confirm:"password", admin:true)
+           u.passwordHashed = u.password.encodeAsPassword()
+           u.save()
+           System.out.println("added default admin")
+          }
+          if (Outcomes.count() == 0 && Classes.count() == 0 && Indicators.count() == 0 && Measures.count() == 0) {
+            def a = new Outcomes(outcomeCategory: 'A', outcomeCategoryDescription: "Students will learn how to...")
+            def b = new Classes(title: "CS481")
+            def c = new Indicators(indicatorName: "a.1", indicatorDescription: "Students will be able to")
+            def d = new Measures(measureTitle: "Exam1_Q1", measureDescription: "Student will be able to")
+            a.addToIndicators(c)
+            c.addToMeasures(d)
+            a.save()
+            b.save()
+        }
+        }
     }
+}
+
     def destroy = {
     }
 }
