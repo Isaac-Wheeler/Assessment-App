cbc7b01ea400f218ff936525014d8dfac58189b5
markymark5127 <markmayne@ymail.com>
2017-11-06 09:24:27 -0500

    fixing merge

--- grails-app/controllers/data/assessment/project/AssessmentsController.groovy
+++ grails-app/controllers/data/assessment/project/AssessmentsController.groovy
@@ -1,21 +1,9 @@
 package data.assessment.project
-<<<<<<< HEAD
-<<<<<<< HEAD
-import grails.validation.Validateable
-import org.springframework.web.multipart.MultipartFile
-=======
-=======
->>>>>>> 489ad93755f942a282dee3d2c4f22d64d8194d60
-
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import com.oreilly.servlet.MultipartRequest;
-<<<<<<< HEAD
->>>>>>> 489ad93755f942a282dee3d2c4f22d64d8194d60
-=======
->>>>>>> 489ad93755f942a282dee3d2c4f22d64d8194d60
 
 class AssessmentsController {
 
@@ -24,24 +12,6 @@ class AssessmentsController {
     def indicators = Indicators.list()
     def classes = Classes.list()
 
-<<<<<<< HEAD
-
-=======
-    if(request instanceof MultipartHttpServletRequest)
-    {
-      MultipartHttpServletRequest mpr = (MultipartHttpServletRequest)request;
-      CommonsMultipartFile f = (CommonsMultipartFile) mpr.getFile("myFile");
-      if(!f.empty)
-        flash.message = 'success'
-      else
-       flash.message = 'file cannot be empty'
-    }
-    else
-      flash.message = 'request is not of type MultipartHttpServletRequest'
-<<<<<<< HEAD
->>>>>>> 489ad93755f942a282dee3d2c4f22d64d8194d60
-=======
->>>>>>> 489ad93755f942a282dee3d2c4f22d64d8194d60
 
     if (request.method == 'POST') {
     def upload() {
