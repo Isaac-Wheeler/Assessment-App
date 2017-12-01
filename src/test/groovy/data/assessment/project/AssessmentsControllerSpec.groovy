ac0b149796cddcc886c1ecf17789f970fcee1a22
kbarley3 <kbarley3@ycp.edu>
2017-12-01 02:15:23 -0500

    A single test case is now running for AD controller!!!
    There is now one test case that runs for the AD controller that tests if a request with correct params and "post" method are set and the index() method is ran on AD controller, than a new AD should be created and saved and retrievable, which this test case shows. More test cases are to come. This test case was to help me learn how to write a controller test case with grails properly.

--- src/test/groovy/data/assessment/project/AssessmentsControllerSpec.groovy
+++ src/test/groovy/data/assessment/project/AssessmentsControllerSpec.groovy
@@ -1,18 +1,69 @@
 package data.assessment.project
 
+import grails.test.mixin.Mock
+import grails.test.mixin.TestFor
+import org.springframework.mock.web.MockMultipartFile
 import grails.testing.web.controllers.ControllerUnitTest
 import spock.lang.Specification
 
-class AssessmentsControllerSpec extends Specification implements ControllerUnitTest<AssessmentsController> {
+
+@TestFor(AssessmentsController)
+@Mock([Outcomes, Indicators, Classes, Teacher, Measures, Document, Assessment_Documentation, Settings])
+class AssessmentsControllerSpec extends Specification {
 
     def setup() {
+      def newSetting = new Settings(academicYear: "2017-2018")
+      newSetting.save(flush:true)
+      def newOutcome = new Outcomes(outcomeCategory: 'A', outcomeCategoryDescription: "Students will learn how to")
+      def newIndicator = new Indicators(indicatorName: "a.1", indicatorDescription: "Students will be able to")
+      def newMeasure = new Measures(measureTitle: "Exam1_Q1", measureDescription: "Student will be able to")
+      newOutcome.addToIndicators(newIndicator)
+      newIndicator.addToMeasures(newMeasure)
+      newOutcome.save(flush:true)
     }
 
     def cleanup() {
     }
 
-    void "test something"() {
-      //TODO do something
-        true
+    void "testing index method for creating a new Assessment_Documentation"() {
+
+          given: "Valid params for AD creation and verify that setup() ran correctly"
+            //verify setup ran correctly before proceeding
+            Settings.count() == 1
+            Outcomes.count() == 1
+            Indicators.count() == 1
+            Measures.count() == 1
+            //if above tests passed then test will proceed with controller test
+
+            params.targetGoal = "75"    //passed as string due to needed parseInt method
+            params.needsImprovement = "10"    //passed as string due to needed parseInt method
+            params.meetsExpectations = "12"   //passed as string due to needed parseInt method
+            params.exceedsExpectations = "8"    //passed as string due to needed parseInt method
+            params.summary = "The target goal was not met"
+            params.assessmentDocTitle = "AD on measure for indicator a.1 on exam1 question 2"
+            params.comments = "Need to focus more on this material!"
+            params.requiredAction = null
+            params.resultComment = "target goal not met!"
+            params.academicSemester = "Fall2017"
+            params.complete = true
+            params.submitButton = "Submit"
+            params.measureID = Measures.first().id    // the id from the only measure that was created in setup()
+
+
+
+
+          when: "The index method is invoked"
+            Assessment_Documentation.count() == 0   // at this time there shouldn't be any AD's since none were saved yet
+            request.method = 'POST'
+            def file = new MockMultipartFile('myFile', 'Hello.docx', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' , "1234567" as byte[])   //create a new file to set as param for "myFile"
+            request.addFile(file)   //add created file to the request under "myFile"
+            controller.index()
+
+
+          then: "The AD should have been saved successfully"
+            Document.count() == 1   // file should have been saved
+            Assessment_Documentation.count() == 1   // new AD should have been saved
+
+
     }
 }
