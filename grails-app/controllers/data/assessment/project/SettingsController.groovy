655a0c7d10dad14f91cc4772235e096d427494c4
Isaac Wheeler <isaacwheeler0@gmail.com>
2017-12-08 12:51:34 -0500

    settings now works

--- grails-app/controllers/data/assessment/project/SettingsController.groovy
+++ grails-app/controllers/data/assessment/project/SettingsController.groovy
@@ -12,46 +12,56 @@ class SettingsController {
           //prevents someone form setting the current year as a new year
           if(params.academicYear != Settings.first().academicYear){
 
-
-
             //does not allow doubles of years
             if(Settings.findByAcademicYear(params.academicYear) == null){
-            def newYear = Settings.first()
-            def oldSettings = new Settings(academicYear:newYear.academicYear)
-            newYear.academicYear = params.academicYear
-            newYear.save(flush:true)
-            oldSettings.save(flush:true)
 
-            def outcomes = Outcomes.findAllByAcademicYear(oldSettings.academicYear)
-            def indicators
-            def oNew
-            def iNew
-            def course
+              def newYear = Settings.first()
+              def oldSettings = new Settings(academicYear:newYear.academicYear)
+              newYear.academicYear = params.academicYear
+              newYear.save(flush:true)
+              oldSettings.save(flush:true)
 
-            outcomes.each{o  ->
-              oNew = new Outcomes(outcomeCategory: o.outcomeCategory, outcomeCategoryDescription: o.outcomeCategoryDescription, academicYear: newYear.academicYear)
-                oNew.save(flush:true)
-              indicators = outcomes.indicators
-              if(indicators != null){
-              indicators.each{ i ->
-                iNew = new Indicators()
-                  iNew.indicatorName = i.indicatorName
-                  iNew.indicatorDescription = i.indicatorDescription
-                  iNew.academicYear = newYear.academicYear
-                  course = Classes.get(i.classes.id)
-                  course.addToIndicators(iNew)
-                  iNew.setOutcome(o)
-                  iNew.save(flush:true)
-              }
-            }
-            }
+              def outcomes = Outcomes.findAllByAcademicYear(oldSettings.academicYear)
+              def indicators
+              def oNew
+              def iNew
+              def course
+
+              outcomes.each{o  ->
+                oNew = new Outcomes(outcomeCategory: o.outcomeCategory, outcomeCategoryDescription: o.outcomeCategoryDescription, academicYear: newYear.academicYear)
+                  oNew.save(flush:true)
+                  indicators = outcomes.indicators
+                  if(indicators != null){
+                    indicators.each{ i ->
+                      iNew = new Indicators()
+                      iNew.indicatorName = i.indicatorName
+                      iNew.indicatorDescription = i.indicatorDescription
+                      iNew.academicYear = newYear.academicYear
+                      course = Classes.get(i.classes.id)
+                      if(course != null){
+                        course.addToIndicators(iNew)
+                      }
+                      iNew.setOutcome(o)
+                      iNew.save(flush:true)
+                    }
+                  }
+                }
           }else{
+
             //updating main if already exists in list
             def settings = Settings.first()
             def oldSettings = new Settings(academicYear:settings.academicYear)
+            def inSettings = Settings.findByAcademicYear(params.academicYear)
+              if(!inSettings.delete(flush:true)){
+                inSettings.errors.allErrors.each { println it }
+              }
             settings.academicYear = params.academicYear
-            settings.save(flush:true)
-            oldSettings.save(flush:true)
+            if(!settings.save(flush:true)){
+              settings.errors.allErrors.each { println it }
+            }
+            if(!oldSettings.save(flush:true)){
+              oldSettings.errors.allErrors.each { println it }
+            }
           }
         }
 
